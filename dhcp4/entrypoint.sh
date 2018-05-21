592ad39ccb7ee1f510c08821aa6b9e90d2dba22b
Matthew Mckenzie <matthew.m.mckenzie@gmail.com>
2018-05-21 10:56:55 +1000

    removing pid files as its not shutting down cleanly

--- dhcp4/entrypoint.sh
+++ dhcp4/entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+rm -f /var/kea/kea-dhcp4.kea-dhcp4.pid
 kea-dhcp4 -c /etc/kea/kea-dhcp4.conf
--- dhcp6/entrypoint.sh
+++ dhcp6/entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+rm -f /var/kea/kea-dhcp6.kea-dhcp6.pid
 kea-dhcp6 -c /etc/kea/kea-dhcp6.conf
